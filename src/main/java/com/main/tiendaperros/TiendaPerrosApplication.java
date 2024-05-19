5b67ce509aa011dceb56c2d604f8045442c4716f
jdhernandez <jurguendark70096@gmail.com>
2024-05-19 15:10:32 -0500

    inicio proyecto

---
+++ src/main/java/com/main/tiendaperros/TiendaPerrosApplication.java
@@ -0,0 +1,13 @@
+package com.main.tiendaperros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaPerrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaPerrosApplication.class, args);
+    }
+
+}
